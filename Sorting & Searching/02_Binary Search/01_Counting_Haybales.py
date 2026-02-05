560a26bc2b96236fecad4001cde25757d0203885
ming <2641976067@qq.com>
2026-02-05 16:27:01 +0800

    2026/02/05 周四 16:27:01.52

---
+++ Sorting & Searching/02_Binary Search/01_Counting_Haybales.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.stdin = open("haybales.in", "r")
+sys.stdout = open("haybales.out", "w")
+
+N, Q = map(int, input().split())
+
+arr = sorted(list(map(int, input().split())))
+
+def at_most(x: int) -> int:
+    lo = 0
+    hi = len(arr)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if arr[mid] <= x:
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo
+
+for _ in range(Q):
+    a, b = map(int, input().split())
+    ans = at_most(b) - at_most(a - 1)
+    print(ans)
