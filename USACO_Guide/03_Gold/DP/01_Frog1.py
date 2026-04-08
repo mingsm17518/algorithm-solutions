17b614b9e729840a91f039c0bec6361583683997
ming <2641976067@qq.com>
2026-04-08 08:16:30 +0800

    2026/04/08 周三  8:16:30.39

--- USACO_Guide/03_Gold/DP/01_Frog1.py
+++ USACO_Guide/03_Gold/DP/01_Frog1.py
@@ -1,14 +1,12 @@
 n = int(input())
-arr = [int(x) for x in input().split()]
-
-dp = [float("inf") for _ in range(n)]
-
+h = list(map(int, input().split()))
+INF = 10**18
+dp = [INF] * n
 dp[0] = 0
 
 for i in range(1, n):
-    if i >= 1:
-        dp[i] = min(dp[i], dp[i - 1] + abs(arr[i] - arr[i - 1]))
+    dp[i] = min(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]))
     if i >= 2:
-        dp[i] = min(dp[i], dp[i - 2] + abs(arr[i] - arr[i - 2]))
-
-print(dp[-1])
+        dp[i] = min(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]))
+    
+print(dp[n - 1])
--- USACO_Guide/03_Gold/DP/02_Dice_Combinations.py
+++ USACO_Guide/03_Gold/DP/02_Dice_Combinations.py
@@ -1,11 +1,11 @@
-MOD = 10**9 + 7
-
 n = int(input())
 
 dp = [1]
 
+MOD = 10 ** 9 + 7
+
 for i in range(n):
-    total = sum(dp[-6:])
-    dp.append(total % MOD)
+    total = sum(dp[-6:]) % MOD
+    dp.append(total)
 
 print(dp[-1])
